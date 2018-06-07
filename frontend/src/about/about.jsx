4e0f6c851fde7bd9b10ddeb1ba265092de3daf70
juliohds <tesjuliohenrique@gmail.com>
2018-06-07 10:50:44 -0300

    app ok

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa Historio</h2>
+        <p>Loren ipson</p>
+        <h2>Missao</h2>
+        <p>minha missao é aprender react</p>
+        <h2>Visao</h2>
+        <p>minha visao é ver que eu aprendi esse framework</p>
+        <h2>Valor</h2>
+        <p>meu valor é o quanto eu sei</p>
+    </div>
+)
